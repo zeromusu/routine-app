b8857cbd9937697ff6bf1fa75619fa7831b682d6
Shoma Hirano <hirano.shoma.99@gmail.com>
2026-01-04 03:00:54 +0900

    Implement routine persistence with error mapping

--- server/internal/domain/repository/routine.go
+++ server/internal/domain/repository/routine.go
@@ -1,4 +1,9 @@
 package repository
 
+import "routine-app-server/internal/domain"
+
+//go:generate mockery --name=RoutineRepository --case=underscore --output=./mocks
+
 type RoutineRepository interface {
+	Create(routine *domain.Routine) error
 }
--- server/internal/infrastructure/persistence/routine.go
+++ server/internal/infrastructure/persistence/routine.go
@@ -1,8 +1,11 @@
 package persistence
 
 import (
+	"errors"
+	"routine-app-server/internal/domain"
 	"routine-app-server/internal/domain/repository"
 
+	"github.com/jackc/pgx/v5/pgconn"
 	"gorm.io/gorm"
 )
 
@@ -12,4 +15,21 @@ type routinePersistence struct {
 
 func NewRoutinePersistence(db *gorm.DB) repository.RoutineRepository {
 	return &routinePersistence{db: db}
+}
+
+func (p *routinePersistence) Create(routine *domain.Routine) error {
+	err := p.db.Create(routine).Error
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgErr.Code == "23505" {
+			return domain.ErrDuplicate
+		}
+
+		if errors.Is(err, gorm.ErrInvalidData) {
+			return domain.ErrInvalidData
+		}
+
+		return domain.ErrDatabase
+	}
+	return nil
 }
